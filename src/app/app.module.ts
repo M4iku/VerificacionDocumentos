8dc0f48fd3a6d355ae657b1428f7ad95f16975e1
m4iku <m4iku@yahoo.com>
2018-12-13 11:19:36 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {ButtonModule, CalendarModule, DropdownModule} from 'primeng/primeng';
-import {InputTextModule} from 'primeng/primeng';
-import {ListboxModule} from 'primeng/primeng';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-
-import {PdfViewerModule} from 'ng2-pdf-viewer';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    ButtonModule,
-    InputTextModule,
-    ListboxModule,
-    FormsModule,
-    ReactiveFormsModule,
-    CalendarModule,
-    DropdownModule,
-    HttpClientModule,
-    PdfViewerModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
